dad9b9ab1dbb776eb9737fb26630d028ecdd7ac5
Haruka Kido <58123962+harukakido@users.noreply.github.com>
2020-02-22 13:29:10 -0500

    Delete In Class Lab 5. Student Names and Overall Average - Copy.py

--- In Class Lab 5. Student Names and Overall Average - Copy.py
+++
@@ -1,9 +0,0 @@
-for student in range(1,4):
-   name = input("Enter Student Name " + str(student)+ ": ")
-   total = 0
-   for val in range(1,4):
-      score = int(input("Enter score " + str(val) + ": "))
-      total += score
-   average = total/3
-print("Average:", average)
-   
